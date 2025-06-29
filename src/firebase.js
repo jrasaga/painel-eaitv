7ad4368401739ed9532801f39a75a1aa73adb84c
jrasaga <65923110+jrasaga@users.noreply.github.com>
2025-06-29 03:13:03 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Configurações do Firebase fornecidas
 const firebaseConfig = {
   apiKey: "AIzaSyAATp8lMCgunWHFRXLdB8MT__i1OoH_9F8",
   authDomain: "painel-eaitv-2fca5.firebaseapp.com",
   projectId: "painel-eaitv-2fca5",
-  storageBucket: "painel-eaitv-2fca5.firebasestorage.app",
+  storageBucket: "painel-eaitv-2fca5.appspot.com", // Corrigido
   messagingSenderId: "394694576972",
   appId: "1:394694576972:web:dc76090b5537a6f394dde9"
 };
 
-// Inicializa o app Firebase e Firestore
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
